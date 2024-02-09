bd7aa722a09e429b179424fd580344aa83a88796
ZiyaSalar <137057250+ZiyaSalar@users.noreply.github.com>
2024-02-09 23:50:32 +0530

    Add files via upload

---
+++ 5. conditional Satement/1_MaxNo.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"Enter first no.: ";
+    cin>>a;
+    cout<<"Enter second no.: ";
+    cin>>b;
+    if(a>b)
+        cout<<"a is greater then b"<<endl;
+    // elif(a=b
+        // cout<<"a is equal to b";
+    else
+        cout<<"a is less than b"<<endl;
+    return 0;
+}
